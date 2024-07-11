cbfe437018ccc72438b31b87ea02a39abc277d88
emily <emillymay@LAPTOP-FPN7UJU6>
2024-07-11 11:24:06 -0600

    setup structure and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+//app.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BookReviewsPage from './pages/BookReviewsPage';
+import PostReviewPage from './pages/PostReviewPage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/reviews" component={BookReviewsPage} />
+          <Route path="/post-review" component={PostReviewPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
